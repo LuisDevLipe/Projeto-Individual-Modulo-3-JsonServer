1e8b59ae82af1d10376441594fac000b1667d330
Luis <squad.10.projetos@gmail.com>
2022-12-14 14:25:50 -0300

    first commit
    ..

---
+++ app.js
@@ -0,0 +1,22 @@
+// const fetch = require("node-fetch");
+
+// fetch('url')
+// .then((result)=>{
+//     console.log(resul);
+// })
+// .catch((erro)=>{
+//     console.log(erro)
+// });
+
+const frutas = ['Banana', 'Uva']
+const [fruta1, fruta2] = frutas
+console.log(frutas,fruta1,fruta2);
+const objComplicado = {
+ arrayProp: [
+ "Resilia",
+ { ramo: "Educação" }
+ ]
+}
+const { arrayProp: [primeiro, segundo ] } = objComplicado
+console.log(objComplicado,primeiro, segundo);
+
---
+++ result.js
@@ -0,0 +1,3 @@
+import { calculaMedia } from './media.js';
+const notasPorBimestre = [7, 9, 4, 9];
+console.log(calculaMedia(notasPorBimestre));
